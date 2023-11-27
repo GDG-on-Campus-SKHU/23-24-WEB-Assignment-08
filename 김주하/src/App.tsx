db5a4df0f1c87b7fd3cadd58a665846f28c1debe
김주하 <101048129+laketree2@users.noreply.github.com>
2023-11-27 11:30:49 +0900

    FIX: 타입 미지정 에러 해결

--- 김주하/src/App.tsx
+++ 김주하/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, ReactElement } from "react";
 import { CallGPT } from "./api/gpt";
 import DiaryInput from "./components/DiaryInput";
 import styled from "styled-components";
@@ -6,42 +6,58 @@ import DiaryDisplay from "./components/DiaryDisplay";
 import { message } from "antd";
 import logo from "./assets/logo.png";
 
-const dummyData = JSON.parse(
-  `{ "title": "개발 고민과 해결", "thumbnail": "https://source.unsplash.com/1600x900/?coding", "summary": "코딩 강의를 듣고 프로젝트에 버그가 발생했지만 해결하지 못하여 GPT를 통해 문제를 해결했음", "emotional_content": "오늘 코딩 강���를 들었는데, 프로젝트에 버그가 많이 나왔어. 스택오버플로에서 검색해봤지만 해결되지 않았어. 그래서 결국 GPT를 통해서 문제를 ���결하게 되었어. 하지만 이렇게 해결하는 것이 내 개발 실력에 도움이 될까 고민이 되는군.", "emotional_result": "이번 상황을 통해 내가 프로그래밍에 대해 더 배울 필요가 있음을 느꼈다. 버그를 해결하는 데에만 의존하는 것보다 개념적으로 이해하고 해결하는 것이 더 중요하다는 것을 깨달았다.", "analysis": "이번 상황은 개발자로서 성장하는 과정에서 마주치는 문제였다. 알고리즘과 문제 해결 능력은 중요하지만, 개념적인 이해와 전체적인 시스템 구조 파악이 더 중요하다는 것을 알 수 있었다. '지식은 힘이다'라는 명언을 생각해보면, 기술적인 도움을 받는 것도 중요하지만 개���적인 이해와 학습은 더 큰 힘이 될 것이다.", "action_list": ["더 깊은 개념적 이해를 ��해 관련 서적을 읽어보기", "다른 개발자들과 소통하여 문제 해결 방법 나누기", "개발자 커뮤니티에 참여하여 지식을 공유하기"] }`
-);
+// dummyData의 타입 정의
+const dummyData: DiaryData = {
+  title: "",
+  thumbnail: "",
+  summary: "",
+  emotional_content: "",
+  emotional_result: "",
+  analysis: "",
+  action_list: [],
+};
 
-function App() {
-  const [data, setData] = useState(dummyData);
+// 타입 정의
+interface DiaryData {
+  title: string;
+  thumbnail: string;
+  summary: string;
+  emotional_content: string;
+  emotional_result: string;
+  analysis: string;
+  action_list: string[];
+}
+
+function App(): ReactElement {
+  // 타입 적용
+  const [data, setData] = useState<DiaryData>(dummyData);
   const [isLoading, setIsLoading] = useState(false);
   const [messageApi, contextHolder] = message.useMessage();
 
-  const handleClickAPICall = async (userInput) => {
+  const handleClickAPICall = async (userInput: string) => {
     try {
       setIsLoading(true);
-      const message = await CallGPT({
-        prompt: `${userInput}`,
-      });
-      setData(JSON.parse(message));
+      // 타입 적용
+      const response = (await CallGPT({ prompt: `${userInput}` })) as DiaryData;
+      setData(response);
     } catch (error) {
-      messageApi.open({
-        type: "error",
-        content: error?.message,
+      messageApi.error({
+        content: (error as Error).message, // 'message' 프로퍼티 사용
       });
-      return;
     } finally {
       setIsLoading(false);
     }
   };
 
-  const handleSubmit = (userInput) => {
+  const handleSubmit = (userInput: string) => {
     handleClickAPICall(userInput);
   };
 
   return (
-    <AppConatiner>
+    <AppContainer>
       {contextHolder}
       <AppTitle>
-        일기 with GPT <img width={"100px"} src={logo}></img>
+        일기 with GPT <img width={"100px"} src={logo} alt="logo"></img>
       </AppTitle>
       <DiaryInput
         messageApi={messageApi}
@@ -51,13 +67,13 @@ function App() {
       <div id="capture">
         <DiaryDisplay isLoading={isLoading} data={data} />
       </div>
-    </AppConatiner>
+    </AppContainer>
   );
 }
 
 export default App;
 
-const AppConatiner = styled.div`
+const AppContainer = styled.div`
   padding: 20px;
   display: flex;
   flex-direction: column;
@@ -73,5 +89,3 @@ const AppTitle = styled.div`
   text-align: center;
   font-family: "Noto Serif KR";
 `;
-
-
