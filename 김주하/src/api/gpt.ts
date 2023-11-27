c90b42fed466e7e5547d7f4c378d11c97a01b353
김주하 <101048129+laketree2@users.noreply.github.com>
2023-11-27 21:17:33 +0900

    FIX: api user 내용 업데이트

--- 김주하/src/api/gpt.ts
+++ 김주하/src/api/gpt.ts
@@ -44,6 +44,30 @@ export const CallGPT = async ({ prompt }: { prompt: string }) => {
         content: `You are a psychological counselor who writes and analyzes emotional diaries. Proceed in the following order.`,
       },
       {
+        role:"user",
+        content: `1. [title] : Think of the diary title after understanding the [events] separated by """ at the bottom.
+        2. [summarize] : summarize events in order with one line sentence.
+        3. [emotional diary] : Write an [emotional diary] with a paragraph based on the summary.
+        4. [evaluates] : The written emotional [evaluates], using explore the unconscious based on the contents of the [emotional diary].
+        6. [Psychological analysis] : Psychological analysis is performed using professional psychological knowledge much more detailed anduse a famous quote.
+        7. [3 action tips] : Write down 3 action tips that will be helpful in the future customer situation. The three action tips must beconverted into JSON Array format.
+        8. [image] : Create an image by making the contents so far into one keyword.
+        
+        
+        Translate into Korean and Use the output in the following JSON format:
+        { 
+            title: here is [title],
+            thumbnail: here is [image],
+            summary: here is [summarize]
+            emotional_content: here is [emotional diary],
+            emotional_result: here is [evaluates],
+            analysis: here is [Psychological analysis],
+            action_list: here is [3 action tips],
+        }
+        
+        [events]:`
+      }
+      {
         role: "user",
         content: `1. [title] : Think of the diary title after understanding the [events] separated by 
           ${prompt}
